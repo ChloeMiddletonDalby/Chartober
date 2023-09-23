e7d62afc4cf9d91d94c01945a9f5082a6b840d9b
Chloe Middleton-Dalby <98756462+ChloeMiddletonDalby@users.noreply.github.com>
2023-09-23 20:05:23 +0100

    Add boxplot

--- 01-Dreams/02-Clean.R
+++ 01-Dreams/02-Clean.R
@@ -1,2 +1,17 @@
 #Chloe
 
+rm(list = ls())
+pacman::p_load(tidyverse, expss, lubridate, gganimate, ggmap, gifski) 
+
+data <- read.csv("01-data.csv")
+
+data %>%
+  ggplot( aes(x=LD, y=AGE, fill=FALSE)) +
+  geom_boxplot() +
+  geom_jitter(color="black", size=0.4, alpha=0.9) +
+  theme(
+    legend.position="none",
+    plot.title = element_text(size=11)
+  ) +
+  ggtitle("A boxplot with jitter") +
+  xlab("")
